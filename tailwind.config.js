ebe04873a8034f2416274462dcfcc5330e1232ea
Ricardo Dias <slowmotion@outlook.com>
2019-11-21 17:18:15 +0000

    First pass on mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        '7xl': '5.125rem',
+      },
+    },
   },
   variants: {},
   plugins: [],
